dc09744ce8250a0f8698e962717860686c7afad9
asathalmannan <ajmalhussainhy@gmail.com>
2020-04-25 17:29:43 +0530

    fixed issues

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,19 +1,15 @@
 "use strict";
 
-var _react = require("react");
+var _react = _interopRequireDefault(require("react"));
 
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 exports.onRenderBody = function (_ref, pluginOptions) {
   var setPostBodyComponents = _ref.setPostBodyComponents;
-  var tawkId = pluginOptions.tawkId
-
-  return setPostBodyComponents([_react2.default.createElement("script", {
+  var tawkId = pluginOptions.tawkId;
+  var source = "https://embed.tawk.to/" + tawkId + "/default";
+  return setPostBodyComponents([/*#__PURE__*/_react["default"].createElement("script", {
     key: "gatsby-plugin-tawk",
-    async: true,
-    src: "https://embed.tawk.to/" + tawkId + "/default",
-    charset: 'UTF-8'
+    src: source
   })]);
 };
--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   var tawkId = pluginOptions.tawkId
-  var source = "https://embed.tawk.to/" + tawkId + "/default",
+  var source = "https://embed.tawk.to/" + tawkId + "/default"
   
   return setPostBodyComponents([
     <script
