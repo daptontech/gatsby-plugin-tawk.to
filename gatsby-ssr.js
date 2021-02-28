5a6b223741bc36fd24d1c1148f613e3f940955a7
Mayur <mayur.gaud@easternenterprise.com>
2021-02-28 18:44:41 +0530

    Fixed the issue

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,7 +7,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 exports.onRenderBody = function (_ref, pluginOptions) {
   var setPostBodyComponents = _ref.setPostBodyComponents;
   var tawkId = pluginOptions.tawkId;
-  var source = "https://embed.tawk.to/" + tawkId + "/default";
+  var source = "https://embed.tawk.to/" + tawkId;
   return setPostBodyComponents([/*#__PURE__*/_react["default"].createElement("script", {
     key: "gatsby-plugin-tawk.to",
     src: source
